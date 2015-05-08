f792fa7c850bb72c63f132a8f19d3007c4a9e58a
CJR501 <crees501@gmail.com>
2015-05-08 08:32:11 +0100

    Update Harmoniser with bass
    Added a bass note to the harmoniser which will play every beat, the
    chords play on every stressed beat.

--- src/org/dodgyjammers/jammypiece/components/Harmoniser.java
+++ src/org/dodgyjammers/jammypiece/components/Harmoniser.java
@@ -6,6 +6,8 @@ import org.dodgyjammers.jammypiece.events.RichMidiEvent;
 import org.dodgyjammers.jammypiece.events.TickEvent;
 import org.dodgyjammers.jammypiece.infra.Consumer;
 import org.dodgyjammers.jammypiece.infra.Distributor;
+import org.dodgyjammers.jammypiece.infra.MachineSpecificConfiguration;
+import org.dodgyjammers.jammypiece.infra.MachineSpecificConfiguration.CfgItem;
 import org.dodgyjammers.jammypiece.infra.Producer;
 import org.dodgyjammers.jammypiece.musickb.Chord;
 import org.dodgyjammers.jammypiece.musickb.Key;
@@ -17,11 +19,21 @@ public class Harmoniser extends Distributor<RichMidiEvent> implements Consumer<R
   private final KeyChangeListener mKeyChangeListener;
 
   private Chord mCurrentChord;
-  private int mHarmonyChannel;
   private Chord mNewChord;
+
+  private int mHarmonyChannel;
+  private int mBassChannel;
 
   private volatile Key mKey;
 
+  /**
+   * Produces a harmony based on the chord selected and the soloist's MIDI
+   * events.
+   * @param xiMelodySource
+   * @param xiChordSource
+   * @param xiKeyDetector
+   * @param xiMetronome
+   */
   public Harmoniser(Producer<RichMidiEvent> xiMelodySource,
                     Producer<ChordChangeEvent> xiChordSource,
                     Producer<KeyChangeEvent> xiKeyDetector,
@@ -30,6 +42,8 @@ public class Harmoniser extends Distributor<RichMidiEvent> implements Consumer<R
     mChordListener = new ChordListener();
     mKeyChangeListener = new KeyChangeListener();
     mMetronomeListener = new MetronomeListener();
+    mHarmonyChannel = MachineSpecificConfiguration.getCfgVal(CfgItem.CHORD_CHANNEL, 0);
+    mBassChannel = MachineSpecificConfiguration.getCfgVal(CfgItem.BASS_CHANNEL, 0);
 
     xiMelodySource.registerConsumer(this);
     xiChordSource.registerConsumer(mChordListener);
@@ -58,8 +72,28 @@ public class Harmoniser extends Distributor<RichMidiEvent> implements Consumer<R
     @Override
     public void consume(TickEvent xiItem)
     {
-	  playNewChord();
+      if(xiItem.mTickInBeat == 0)
+      {
+        playNewBassNote();
+      }
+
+	    if (xiItem.mStress)
+	    {
+	      playNewChord();
+	    }
     }
+  }
+
+  public void playNewBassNote()
+  {
+    if (mCurrentChord != null)
+    {
+      int lCurrentBassNote = getPlayableBass(mCurrentChord);
+      distribute(RichMidiEvent.makeNoteOff(mBassChannel, lCurrentBassNote));
+    }
+
+    int lNewBassNote = getPlayableBass(mNewChord);
+    distribute(RichMidiEvent.makeNoteOn(mBassChannel, lNewBassNote));
   }
 
   public void playNewChord()
@@ -103,5 +137,11 @@ public class Harmoniser extends Distributor<RichMidiEvent> implements Consumer<R
     {
       mKey = xiKey.mNewKey;
     }
+  }
+
+  private int getPlayableBass(Chord xiChord)
+  {
+    int lBassNote = xiChord.getBassNote() + mKey.mTonicNoteNum;
+    return lBassNote;
   }
 }
